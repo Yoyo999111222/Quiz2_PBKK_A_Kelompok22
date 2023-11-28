55f147fb9bd501016bcbb0a3fb9df3845cfb9f23
Yoyo999111222 <frederickyonatan111@gmail.com>
2023-11-29 00:57:49 +0700

    edit style

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -43,8 +43,8 @@
           <h5 class="card-title mb-1"><?php echo $brg->nama_brg ?></h5>
           <small><?php echo $brg->keterangan ?></small>
           <br>
-          <span class="badge badge-pill badge-light-purple mb-3">Rp<?php echo number_format($brg->harga, 0, ',', '.') ?></span>
-          <?php echo anchor('dashboard/tambah_ke_keranjang/' . $brg->id_brg, '<div class="btn btn-sm btn-primary add-to-cart">Tambah ke Keranjang</div>') ?>
+          <span class="badge badge-pill badge-light-purple mb-3">Rp<?php echo number_format($brg->harga, 0, ',', '.') ?></span><br>
+          <?php echo anchor('dashboard/tambah_ke_keranjang/' . $brg->id_brg, '<div class="btn btn-sm btn-primary add-to-cart">Beli Tiket</div>') ?>
           <?php echo anchor('dashboard/detail/' . $brg->id_brg, '<div class="btn btn-sm btn-cyan view-details">Detail</div>') ?>
         </div>
       </div>
