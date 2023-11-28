67890b85a11baf529b4493bb6c442c96af3b1cf4
Yoyo999111222 <frederickyonatan111@gmail.com>
2023-11-28 21:43:31 +0700

    continue from richie searchbar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -81,7 +81,7 @@
           </button>
 
           <!-- Topbar Search -->
-          <form action="<?= base_url() . 'search' ?>" class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+          <form action="<?= base_url() . 'welcome/search' ?>" class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
             <div class="input-group">
               <input name="search" type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
               <div class="input-group-append">
