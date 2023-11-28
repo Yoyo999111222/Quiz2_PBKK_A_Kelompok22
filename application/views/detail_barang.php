53acb358182252b6650c95d9654542bd84b83313
deekuh <andhikalingga2003@gmail.com>
2023-11-28 23:22:33 +0700

    watch trailer works

--- application/views/detail_barang.php
+++ application/views/detail_barang.php
@@ -57,30 +57,57 @@
             <?php echo anchor('welcome/index/', '<div class="btn btn-sm btn-danger">Kembali</div>') ?>
             <div class="btn btn-sm btn-success" data-toggle="modal" data-target="#videoModal">Watch Trailer</div>
     
-              <!-- Modal -->
-              <div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-labelledby="videoModalLabel" aria-hidden="true">
-                  <div class="modal-dialog" role="document">
-                      <div class="modal-content">
-                          <div class="modal-header">
-                              <h5 class="modal-title" id="videoModalLabel">YouTube Video Popup</h5>
-                              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                  <span aria-hidden="true">&times;</span>
-                              </button>
-                          </div>
-                          <div class="modal-body">
-                              <!-- Embed YouTube video iframe -->
-                              <iframe width="100%" height="315" src="https://www.youtube.com/watch?v=uYPbbksJxIg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                          </div>
-                          <div class="modal-footer">
-                              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                          </div>
+            <!-- Modal -->
+          <div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-labelledby="videoModalLabel" aria-hidden="true">
+              <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <h5 class="modal-title" id="videoModalLabel">YouTube Video Popup</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                          </button>
+                      </div>
+                      <div class="modal-body">
+                          <!-- Embed YouTube video container -->
+                          <div id="youtubeVideo"></div>
+                      </div>
+                      <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                       </div>
                   </div>
               </div>
+          </div>
 
-              <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-              <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-              <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+          <script>
+              $(document).ready(function () {
+                  // Load YouTube API script dynamically
+                  var tag = document.createElement('script');
+                  tag.src = 'https://www.youtube.com/iframe_api';
+                  var firstScriptTag = document.getElementsByTagName('script')[0];
+                  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+                  // Define the onYouTubeIframeAPIReady function
+                  window.onYouTubeIframeAPIReady = function () {
+                      // Create a new YouTube player
+                      new YT.Player('youtubeVideo', {
+                          width: '100%',
+                          height: '315',
+                          videoId: 'uYPbbksJxIg',
+                          playerVars: {
+                              'autoplay': 0,
+                              'controls': 1,
+                              'showinfo': 0,
+                              'rel': 0,
+                              'modestbranding': 1
+                          }
+                      });
+                  };
+              });
+          </script>
     
           </div>
         </div>
